18dcdfabcca80854a14e9c3894801344607c5bbf
maxwell-rosen <mrquell@gmail.com>
2026-06-12 21:43:09 +0000

    Remove fast math, loosen tolerances on some moments unit tests

--- moments/unit/ctest_wv_gr_ultra_rel_euler.c
+++ moments/unit/ctest_wv_gr_ultra_rel_euler.c
@@ -1116,7 +1116,7 @@ test_gr_ultra_rel_euler_waves_schwarzschild()
           gkyl_wv_eqn_rotate_to_global(gr_ultra_rel_euler, tau1[d], tau2[d], norm[d], fr_local, fr);
 
           for (int i = 0; i < 70; i++) {
-            TEST_CHECK( gkyl_compare(fr[i] - fl[i], amdq[i] + apdq[i], 1e-12) );
+            TEST_CHECK( gkyl_compare(fr[i] - fl[i], amdq[i] + apdq[i], 5e-12) );
           }
         }
       }
